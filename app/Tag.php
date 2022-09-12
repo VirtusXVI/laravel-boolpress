3ab7c63d2c9bb411b1809a035d0c141259025822
VirtusXVI <manuel.aveta2001@gmail.com>
2022-09-12 15:35:43 +0200

    checkpoint 1 update

--- app/Tag.php
+++ app/Tag.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    public function category() {
+    public function post() {
         return $this->belongsToMany('App\Posts');
     }
 }
